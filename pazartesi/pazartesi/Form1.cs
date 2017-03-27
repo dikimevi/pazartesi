0e49e452d8e3d46fd902c030170ee7d4129398a3
dikimevipersonel <dikimevipersonel@gmail.com>
2017-03-27 10:47:43 +0300

    İkinci commit

--- pazartesi/pazartesi/Form1.cs
+++ pazartesi/pazartesi/Form1.cs
@@ -16,5 +16,10 @@ namespace pazartesi
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("değişiklik");
+        }
     }
 }
